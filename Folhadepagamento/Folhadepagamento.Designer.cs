f98dda92df988714ea04ae556f9c12e9ea4801ee
unknown <robertopsantos85@gmail.com>
2019-02-10 10:58:24 -0200

    Commit inicial: Teste de utilização do Git

---
+++ Folhadepagamento/Folhadepagamento.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Folhadepagamento
+{
+    partial class Folhadepagamento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblSB = new System.Windows.Forms.TextBox();
+            this.lblINSS = new System.Windows.Forms.TextBox();
+            this.lblIR = new System.Windows.Forms.TextBox();
+            this.lblSL = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtHT = new System.Windows.Forms.TextBox();
+            this.txtVH = new System.Windows.Forms.TextBox();
+            this.txtDep = new System.Windows.Forms.TextBox();
+            this.btnCalcula = new System.Windows.Forms.Button();
+            this.btnLimpa = new System.Windows.Forms.Button();
+            this.btnFim = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblSB);
+            this.panel1.Controls.Add(this.lblINSS);
+            this.panel1.Controls.Add(this.lblIR);
+            this.panel1.Controls.Add(this.lblSL);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Location = new System.Drawing.Point(59, 208);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(329, 143);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblSB
+            // 
+            this.lblSB.Enabled = false;
+            this.lblSB.Location = new System.Drawing.Point(148, 13);
+            this.lblSB.Name = "lblSB";
+            this.lblSB.ReadOnly = true;
+            this.lblSB.Size = new System.Drawing.Size(100, 20);
+            this.lblSB.TabIndex = 0;
+            // 
+            // lblINSS
+            // 
+            this.lblINSS.Enabled = false;
+            this.lblINSS.Location = new System.Drawing.Point(148, 42);
+            this.lblINSS.Name = "lblINSS";
+            this.lblINSS.ReadOnly = true;
+            this.lblINSS.Size = new System.Drawing.Size(100, 20);
+            this.lblINSS.TabIndex = 0;
+            // 
+            // lblIR
+            // 
+            this.lblIR.Enabled = false;
+            this.lblIR.Location = new System.Drawing.Point(148, 72);
+            this.lblIR.Name = "lblIR";
+            this.lblIR.ReadOnly = true;
+            this.lblIR.Size = new System.Drawing.Size(100, 20);
+            this.lblIR.TabIndex = 0;
+            // 
+            // lblSL
+            // 
+            this.lblSL.Enabled = false;
+            this.lblSL.Location = new System.Drawing.Point(148, 105);
+            this.lblSL.Name = "lblSL";
+            this.lblSL.ReadOnly = true;
+            this.lblSL.Size = new System.Drawing.Size(100, 20);
+            this.lblSL.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Salário bruto";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 49);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "INSS";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 79);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Imposto de Renda";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 112);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Salário líquido";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome do Funcionário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Horas trabalhadas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Valor da hora";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Num. dependentes";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(139, 23);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(266, 20);
+            this.txtNome.TabIndex = 1;
+            this.txtNome.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNome_KeyPress);
+            // 
+            // txtHT
+            // 
+            this.txtHT.Location = new System.Drawing.Point(139, 52);
+            this.txtHT.Name = "txtHT";
+            this.txtHT.Size = new System.Drawing.Size(100, 20);
+            this.txtHT.TabIndex = 2;
+            this.txtHT.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtHT_KeyPress);
+            // 
+            // txtVH
+            // 
+            this.txtVH.Location = new System.Drawing.Point(139, 83);
+            this.txtVH.Name = "txtVH";
+            this.txtVH.Size = new System.Drawing.Size(100, 20);
+            this.txtVH.TabIndex = 3;
+            this.txtVH.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtVH_KeyPress);
+            // 
+            // txtDep
+            // 
+            this.txtDep.Location = new System.Drawing.Point(139, 115);
+            this.txtDep.Name = "txtDep";
+            this.txtDep.Size = new System.Drawing.Size(100, 20);
+            this.txtDep.TabIndex = 4;
+            this.txtDep.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDep_KeyPress);
+            // 
+            // btnCalcula
+            // 
+            this.btnCalcula.Location = new System.Drawing.Point(59, 157);
+            this.btnCalcula.Name = "btnCalcula";
+            this.btnCalcula.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcula.TabIndex = 13;
+            this.btnCalcula.Text = "Calcular";
+            this.btnCalcula.UseVisualStyleBackColor = true;
+            this.btnCalcula.Click += new System.EventHandler(this.btnCalcula_Click);
+            // 
+            // btnLimpa
+            // 
+            this.btnLimpa.Location = new System.Drawing.Point(180, 157);
+            this.btnLimpa.Name = "btnLimpa";
+            this.btnLimpa.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpa.TabIndex = 14;
+            this.btnLimpa.Text = "Limpar";
+            this.btnLimpa.UseVisualStyleBackColor = true;
+            this.btnLimpa.Click += new System.EventHandler(this.btnLimpa_Click);
+            // 
+            // btnFim
+            // 
+            this.btnFim.Location = new System.Drawing.Point(313, 157);
+            this.btnFim.Name = "btnFim";
+            this.btnFim.Size = new System.Drawing.Size(75, 23);
+            this.btnFim.TabIndex = 15;
+            this.btnFim.Text = "Fim";
+            this.btnFim.UseVisualStyleBackColor = true;
+            this.btnFim.Click += new System.EventHandler(this.btnFim_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 363);
+            this.Controls.Add(this.btnCalcula);
+            this.Controls.Add(this.btnLimpa);
+            this.Controls.Add(this.btnFim);
+            this.Controls.Add(this.txtDep);
+            this.Controls.Add(this.txtVH);
+            this.Controls.Add(this.txtHT);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Folha de pagamento";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox lblSB;
+        private System.Windows.Forms.TextBox lblINSS;
+        private System.Windows.Forms.TextBox lblIR;
+        private System.Windows.Forms.TextBox lblSL;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtHT;
+        private System.Windows.Forms.TextBox txtVH;
+        private System.Windows.Forms.TextBox txtDep;
+        private System.Windows.Forms.Button btnCalcula;
+        private System.Windows.Forms.Button btnLimpa;
+        private System.Windows.Forms.Button btnFim;
+    }
+}
+
---
+++ Folhadepagamento/Taxas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Folhadepagamento
+{
+    public static class Taxas
+    {
+       public static double CalculaINSS(double salBruto)
+        {
+            if (salBruto <= 965.67) return salBruto * 0.08;
+            else if (salBruto <= 1609.45) return salBruto * 0.09;
+            else if (salBruto <= 3218.90) return salBruto * 0.11;
+            else return (354.08);
+
+        }
+
+        public static double CalculaIR(double salIR)
+        {
+            if (salIR <= 1434.59) return (0);
+            else if (salIR <= 2150.00) return salIR * 0.075 - 107.59;
+            else if (salIR <= 2866.70) return salIR * 0.15 - 268.84;
+            else if (salIR <= 3582.00) return salIR * 0.225 - 483.84;
+            else return salIR * 0.275 - 662.94;
+        }
+    }
+}
